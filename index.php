a988371968486a6bf73f0984ccff54e69f7c0da4
sangeetha21 <sangeethasn2000@gmail.com>
2021-07-16 23:49:42 +0530

    Uploaded on github

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Basic Banking System</title>
-    <link rel="stylesheet" href="css/home_page.css">
+    <link rel="stylesheet" href="css/index_page.css">
     <!--Bootstrap and google fonts links -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
